dae584fa7642bfbf83a38633c6166aceb313377d
SajivSingh <sajivraj53515@gmail.com>
2022-12-04 23:54:44 +0530

    Add arrays and test

---
+++ LeetCodeStudy.Tests/Learn/Arrays101/Arrays101Tests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCodeStudy.Learn.Arrays101.Tests
+{
+    [TestClass()]
+    public class Arrays101Tests
+    {
+        private MaxConsecutiveOnes _maxConsecutiveOnes;
+
+        public Arrays101Tests()
+        {
+            _maxConsecutiveOnes = new MaxConsecutiveOnes();
+        }
+
+        [DataRow(new int[] { 1, 1, 0, 1, 1, 1 }, 3)]
+        [TestMethod]
+        public void FindMaxConsecutiveOnes(int[] nums, int result)
+        {
+            int maxCount = _maxConsecutiveOnes.FindMaxConsecutiveOnes(nums);
+            Assert.AreEqual(maxCount, result);
+        }
+    }
+}
---
+++ LeetCodeStudy/Learn/Arrays101/MaxConsecutiveOnes.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeStudy.Learn.Arrays101
+{
+    public class MaxConsecutiveOnes
+    {
+        public int FindMaxConsecutiveOnes(int[] nums)
+        {
+            int max = 0;
+            int count = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == 1)
+                {
+                    count++;
+                    if (max < count)
+                        max = count;
+                }
+                else
+                {
+                    count = 0;
+                }
+
+            }
+            return max;
+        }
+    }
+}
